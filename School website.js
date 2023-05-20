f811fca7a6cd86c96ee839d0e462bd65552bd226
Unish Rai <kirantunish@gmail.com>
2023-05-20 14:55:52 +0545

    first commit

---
+++ School website.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', function(){
+    let navbar=this.document.querySelector('nav');
+    if(window.pageYOffset>=300){
+        navbar.classList.add('nav-scroll')
+    }
+    else{
+        navbar.classList.remove('nav-scroll')
+    }
+})
+
+
+
+//open the frequently ask question 
+const faqs=document.querySelectorAll('.faq');
+
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+    })
+})
+
+
+//show / nav menu
+const menu=document.querySelector(".nav-menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+const showHide=document.querySelector(".container a h3")
+
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+    showHide.style.display="none"
+
+})
+
+//close nav menu
+const closeNav=()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+    showHide.style.display="inline-block"
+}
+closeBtn.addEventListener('click',closeNav)
